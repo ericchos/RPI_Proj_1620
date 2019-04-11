646c280409424b7888f2a941bb797de92266ff55
Eric-Macbook <ericchos@gmail.com>
2019-04-10 19:22:09 -0700

    Digital Sensor Input
    - Added digital sensor input to the program
    - Added filtering function for noise spikes.
    - Tested on hardware, but needed to be tested on actual application on motors.

--- Phase_Converter_Arduino/Phase_Converter_Arduino.ino
+++ Phase_Converter_Arduino/Phase_Converter_Arduino.ino
@@ -11,19 +11,23 @@
  * https://www.github.com/ericchos/RPI_Proj_1620
 **********************************************************************************************/
 /*
- * SETTINGS
+ * Defines whether Serial output will be used. 
+ * Comment out if not using 
  */
-#define DEBUG //Defines whether debug output will be used. Comment out if not using 
-//#define PZEM_SENSOR 1
-//#define ANALOG_DETECT 1
-#define ONOFF_DETECT  1
- 
+#define DEBUG 
 #ifdef DEBUG 
   #define D(x) Serial.print(x)
 #else
   #define D(x)
 #endif
 
+/*
+ * Uncomment one option for Load Current detection 
+ */
+//#define PZEM_SENSOR 1
+//#define ANALOG_DETECT 1
+#define ONOFF_DETECT  1
+ 
 // Include Libraries
 #ifdef PZEM_SENSOR
 #include <PZEM004T.h>
@@ -33,15 +37,15 @@ IPAddress ip(192,168,1,1);
 #endif
 
 // Arduino Pin Definitions
-const byte IDLER_VTG = A0;          //Analog Input To Pin A0
+const byte LOAD_STATE = A0;         //Input <- From Current Sensor. Only On/Off signal given to detect current
 const byte RPIGPIO_20_START = A1;   //Input <- From Rapsberry PI GPIO20
 const byte RPIGPIO_21_FINISH = A2;  //Input <- From Rapsberry PI GPIO21
-const byte IDLER_STATE = 10;        //Input <- To Pin D10
-const byte UNUSED_RELAY = 6;        //Output -> To Pin D6 
+const byte IDLER_STATE = 10;        //Input <- To Pin D10. A On/Off signal given when Idler starts
 const byte LOAD_RELAY = 3;          //Output -> To Pin D3
 const byte IDLER_RELAY = 4;         //Output -> To Pin D5
-const byte STARTCAP_RELAY = 5;      //Output -> To Pin D4
 const byte LED = 13;                //Output -> To Pin D13 Arduino Onboard LED
+//const byte UNUSED_RELAY = 6;      //UNUSED Output -> To Pin D6
+//const byte STARTCAP_RELAY = 5;    //UNUSED Output -> To Pin D4
 
 // Set Constants used for triggering values
 const int IDLER_ON_LIMIT = 0; //VAC
@@ -49,7 +53,7 @@ const int IDLER_OFF_LIMIT = 0; //VAC
 const float SETPOINT = 1.0;
 
 // Global Variables
-unsigned int loadSensor = 0; // Value of the Load Current used to trigger relay sequence
+float loadSensor = 0; // Value of the Load Current used to trigger relay sequence
 
 // Set Up Routine.Runs first in the Program
 void setup() {
@@ -58,22 +62,26 @@ void setup() {
 
   // Start Arduino to PC Serial Comm
   Serial.begin(9600);
+  D("- Starting Phase Converter Program -\r\n");
   
-  // Set Arduino GPIO pins to Output
+  // Set Arduino GPIO Input Pins
   pinMode(RPIGPIO_20_START, INPUT);
   pinMode(RPIGPIO_21_FINISH, INPUT);
-  pinMode(LOAD_RELAY, OUTPUT);
-  pinMode(STARTCAP_RELAY, OUTPUT);
-  pinMode(IDLER_RELAY, OUTPUT);
-  pinMode(UNUSED_RELAY, OUTPUT);
   pinMode(IDLER_STATE, INPUT);
+  pinMode(LOAD_STATE, INPUT);
+
+  // Set Arduino GPIO Output Pins
+  pinMode(LOAD_RELAY, OUTPUT);
+  pinMode(IDLER_RELAY, OUTPUT);
   pinMode(LED, OUTPUT);
+  //pinMode(UNUSED_RELAY, OUTPUT); // Relay D6
+  //pinMode(STARTCAP_RELAY, OUTPUT); // Relay D5
   
   // Set Default States for the Relays
   digitalWrite(LOAD_RELAY, HIGH);   // EXTERNAL EQUIPMENT. NORMALLY CLOSED
-  digitalWrite(STARTCAP_RELAY, LOW);  // START CAPACITORS
   digitalWrite(IDLER_RELAY, LOW);  // IDLER MOTOR
-  digitalWrite(UNUSED_RELAY, LOW);  // Unused Relay
+  //digitalWrite(STARTCAP_RELAY, LOW);  // START CAPACITORS
+  //digitalWrite(UNUSED_RELAY, LOW);  // Unused Relay
 }
 
 // Main Loop. This repeats over the whole operation.
@@ -86,16 +94,18 @@ void loop() {
 #elif ANALOG_DETECT 
   loadSensor = analogRead(A0);
 #elif ONOFF_DETECT 
-  loadSensor = digitalRead(A0);
+  loadSensor = readAverage(A0, 1000);
 #endif
-  D(loadSensor + "\r\n");
 
 /*
  * Step 2: Check if Load amperage is detected
  */
-  if(loadSensor >= SETPOINT || loadSensor == true){
+  if(loadSensor < SETPOINT){
     digitalWrite(LED, HIGH);
-    D("Step 1: Shut down load. Start Capaciter & Idler\r\n");
+    D("Step 1: Shut down load and start idler.\r\n");
+    D("\t Load Signal: ");
+    D(loadSensor);
+    D(" (1 if OFF, Less than 1 is ON)\r\n");
     // Turn off the load relay and switch on the start caps
     digitalWrite(LOAD_RELAY, LOW);
     digitalWrite(IDLER_RELAY, HIGH);
@@ -106,50 +116,68 @@ void loop() {
  * turn off the start capcacitors and turn on the load
  * relay to run off the load power source
  */
-    D("Step 2: Waiting for Idler Voltage to Rise\r\n");
-    while(digitalRead(IDLER_STATE) != true)
-      delay(10);
-    delay(500);
-    D("Step 4: Ider Voltage has gone up. Turning off Start Caps & switching back to load\r\n");
+    D("Step 2: Waiting for Idler Voltage to Rise by taking averages of the start capacitor output Signal.\r\n");
+    float idlerState = 0.0;
+    do{
+      idlerState = readAverage(IDLER_STATE, 1000);
+    }while(idlerState < SETPOINT);
+    D("Step 3: Ider Voltage has gone up. Turning off Start Caps & switching back to load.\r\n");
+    D("\t Idler State: ");
+    D(idlerState);
+    D("(1 is ON. Less than 1 is OFF)\r\n");
     digitalWrite(LOAD_RELAY, HIGH);
+    digitalWrite(IDLER_RELAY, HIGH);
     
 /*
  * Step 4: If the Raspberry Pi detects low current due to the 
  * user shutting off the equipment, the load circuit
  * will be disconnected and the idler motor will continue to run.
  */
+  D("Step 4: Unit will run until load current drops when user powers off their equipment\r\n");
 #ifdef PZEM_SENSOR
     do{
       loadSensor = (unsigned int)pzem.current(ip);
-      D(loadSensor + "\r\n");
       delay(10);
     }while(i > 1.0);
 #elif ANALOG_DETECT
-    while(analogRead(A0) <= SETPOINT);
+    while(analogRead(A0) != 1.0);
 #elif ONOFF_DETECT
-    while(digitalRead(A0) != false);
+  // Wait for the Load Sensor Output signal to drop
+  float loadState = 0.0f;
+  do{
+    loadState = readAverage(A0, 1000);
+  }while(loadState < 1.0);
 #endif
 
 /*
  * Step 5: Shut down sequence
  */
     D("Step 5: Shut Down Sequence.\r\n");
+    D("\t Load State: ");
+    D(loadState);
+    D(" (1 is OFF. Less than 1 is ON)\r\n");
     digitalWrite(LOAD_RELAY, HIGH);
     digitalWrite(IDLER_RELAY, LOW);
     delay(100);
     digitalWrite(LED, LOW);
+
+    D("- Back to Start. Waiting for Load Current -\r\n\r\n");
   }
 }
 
 /*
- * Take analog readings and average them
+ * Take averages of the readings
  */
-float analogAverage(int pin){
+float readAverage(int pin, int samples){
   float average = 0;
-  for(int i = 0; i<300; i++){
-    average += analogRead(pin);
+  for(int i = 0; i<samples; i++){
+    #ifdef ANALOG_DETECT
+    average += (float)analogRead(pin);
+    #elif ONOFF_DETECT
+    average += digitalRead(pin);
+    #endif
   }
-  return average = (float)average/300.0;
+  return average = average/(float)samples;
 }
 
 /*
